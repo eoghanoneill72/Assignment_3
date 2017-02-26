55d76258cf3d0891068e2e127b0a242126927de3
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-02-26 15:48:44 +0000

    toggle() function investigation, boolean negation and reassignment not working.

--- src/main.py
+++ src/main.py
@@ -1,32 +1,5 @@
-import random
-
-n = 10
-a = [[True]*n for i in range(n)]
-# b = [False]*n*n
-
-# total = 0
-# for e in a:
-#     if e == True:
-#         total+=(sum(e))
-# print(total)
- 
- 
-
-b = [True if random.random() < 0.5 else False for _ in range(10)]
-print("b is", b)
-
-# compute the sum (only counts elements which are True)
-def count():
-    print(sum(b))
-
-
-# b = [val for val in a if val == True]
-print("b is: ",b)
-
-# for val in a:
-#     print(val)
-    
-print(b.count(True))
+def set_up(n):
+    return[[False]*n for i in range(n)]
 
 def valid_parameters(x1,y1,x2,y2):
     if x1 > n:
@@ -45,24 +18,49 @@ def valid_parameters(x1,y1,x2,y2):
         y1 = 0
     if y2 < 0:
         y2 = 0
-    return x1,y1,y2,y2
+    return x1,y1,x2,y2
 
-# def switch(a):
-#     valid_parameters(x1, y1, x2, y2)
-#     x = max(x1,x2)
-#     y = max(y1,y2)
-#     [not i for i in range a]
-    
-def toggle(a):
-    a = not a 
-    return a 
+def toggle(e):
+    e = not e
+    return e
+        
+def turn_on(x1,y1,x2,y2):
+    valid_parameters(x1,y1,x2,y2)
+    for i in range(min(y1,y2),max(y1,y2)):
+        for j in range(min(x1,x2),max(x1,x2)):
+            if a[i][j] == False:
+                toggle(a[i][j])
+        
  
 def switch(x1,y1,x2,y2):
+    valid_parameters(x1,y1,x2,y2)
     for i in range(min(y1,y2),max(y1,y2)):
         for j in range(min(x1,x2),max(x1,x2)):
             toggle(a[i][j])
+            
+def turn_off(x1,y1,x2,y2):
+    valid_parameters(x1,y1,x2,y2)
+    for i in range(min(y1,y2),max(y1,y2)):
+        for j in range(min(x1,x2),max(x1,x2)):
+            if a[i][j] == True:
+                toggle(a[i][j])
 
-def create_grid(n):
-    [[True]*n*n]
-    
-print(create_grid(6))
+def count():
+    print(sum([val for val in a if val == True]))
+
+# def create_grid(n):
+#     [[False]*n for j in range(n)]
+#     b = create_grid(n)
+#     return b
+# 
+# def grid(n):
+#     return [[False for i in range(n)] for j in range(n)]
+n = 5
+a = set_up(n)
+
+print(a)
+print(a[1][1])
+toggle(a[1][1])
+print(a[1][1])
+count()
+
