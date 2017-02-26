5fb922312948da70fa845110c1029f9edc2ab104
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-02-26 13:43:56 +0000

    2D list initialisation,
    Error with sum() method to quantify booleans

---
+++ src/main.py
@@ -0,0 +1,22 @@
+n = 10
+a = [[True]*n for i in range(n)]
+
+total = 0
+for val in a:
+    if val = True
+    total+=(sum(val))
+print(total)
+ 
+a[0][1]=False
+
+total = 0
+# b = [val for val in a if val == True]
+print(b)
+
+# for val in a:
+#     print(val)
+    
+print(a.count(True))
+
+def create_grid(n):
+    [[True]*n*n]
