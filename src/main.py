e07c47a956239aecccb5ff743063efd1ffdab5e6
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-02-28 17:33:14 +0000

    main and test
    main printing to console. count is incorrect for some reason.
    arguments are correct.
    core functions must not be correct?

--- src/main.py
+++ src/main.py
@@ -7,14 +7,14 @@ class led:
         self.a = [[False]*self.size for i in range(self.size)]
     
     def valid_parameters(self,x1,y1,x2,y2):
-        if x1 > self.size:
-            x1 = self.size
-        if x2 > self.size:
-            x2 = self.size
-        if y1 > self.size:
-            y1 = self.size
-        if y2 > self.size:
-            y2 = self.size
+        if x1 > self.size-1:
+            x1 = self.size-1
+        if x2 > self.size-1:
+            x2 = self.size-1
+        if y1 > self.size-1:
+            y1 = self.size-1
+        if y2 > self.size-1:
+            y2 = self.size-1
         if x1 < 0:
             x1 = 0
         if x2 < 0:
@@ -31,40 +31,52 @@ class led:
             
     def turn_on(self,x1,y1,x2,y2):
         x1,y1,x2,y2 = self.valid_parameters(x1,y1,x2,y2)
-        for i in range(min(y1,y2),max(y1,y2)):
-            for j in range(min(x1,x2),max(x1,x2)):
+        for i in range(min(y1,y2),max(y1,y2)+1):
+            for j in range(min(x1,x2),max(x1,x2)+1):
                 if self.a[i][j] == False:
-                    self.toggle(self.a[i][j])
+                    self.a[i][j]=True
+                    #self.toggle(self.a[i][j])
             
      
     def switch(self,x1,y1,x2,y2):
         x1,y1,x2,y2 = self.valid_parameters(x1,y1,x2,y2)
-        for i in range(min(y1,y2),max(y1,y2)):
-            for j in range(min(x1,x2),max(x1,x2)):
-                self.toggle(self.a[i][j])
+        for i in range(min(y1,y2),max(y1,y2)+1):
+            for j in range(min(x1,x2),max(x1,x2)+1):
+                if self.a[i][j] == False:
+                    self.a[i][j]=True
+                if self.a[i][j] == True:
+                    self.a[i][j]=False
+                #self.toggle(self.a[i][j])
                 
     def turn_off(self,x1,y1,x2,y2):
         x1,y1,x2,y2 = self.valid_parameters(x1,y1,x2,y2)
-        for i in range(min(y1,y2),max(y1,y2)):
-            for j in range(min(x1,x2),max(x1,x2)):
+        for i in range(min(y1,y2),max(y1,y2)+1):
+            for j in range(min(x1,x2),max(x1,x2)+1):
                 if self.a[i][j] == True:
-                    self.toggle(self.a[i][j])
-    
+                    self.a[i][j]=False
+                    #self.toggle(self.a[i][j])
     def count(self):
 #         return(sum([val for val in self.a() if val == True]))
-        tally=0
+#         return np.sum(self.a)
+        tally_false,tally_true,tally_other = 0, 0, 0
         for i in range(self.size):
             for j in range(self.size):
                 if self.a[i][j] == True:
-                    tally+=1
-        return tally
+                    tally_true+=1
+                elif self.a[i][j] == False:
+                    tally_false+=1
+                else:
+                    tally_other
+                    
+        print(self.size)
+        return tally_true, tally_false, tally_other
      
 filename = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
 
 def read_file(filename):
     if filename.startswith("http"):
         fh = urllib.request.urlopen(filename)
-        file_str = fh.read().decode()
+        file_str = fh.read().decode('utf-8')
         return file_str
     else:
         if not os.path.isfile(filename):
@@ -83,21 +95,23 @@ def main():
     
     lights = led(size)
     
+    i=0
     for line in lines:
+        i+=1
         if 'turn on' in line:
             coord1, coord2 = line.split(" ")[2::2]
             x1,y1 = coord1.split(",")
             x1,y1 = int(x1),int(y1)
             x2,y2 = coord2.split(",")
             x2,y2 = int(x2),int(y2)
-            lights.turn_off(x1, y1, x2, y2)
+            lights.turn_on(x1, y1, x2, y2)
         elif "turn off" in line:
             coord1, coord2 = line.split(" ")[2::2]
             x1,y1 = coord1.split(",")
             x1,y1 = int(x1),int(y1)
             x2,y2 = coord2.split(",")
             x2,y2 = int(x2),int(y2)
-            lights.turn_on(x1, y1, x2, y2)
+            lights.turn_off(x1, y1, x2, y2)
         elif "switch" in line:
             coord1, coord2 = line.split(" ")[1::2]
             x1,y1 = coord1.split(",")
@@ -105,16 +119,8 @@ def main():
             x2,y2 = coord2.split(",")
             x2,y2 = int(x2),int(y2)
             lights.switch(x1, y1, x2, y2)
-        
+    print(i)  
     print(lights.count())
-        
-main()
 
-
-
-
-    
-
-    
-    
-
+if __name__ == "__main__":    
+    main()
--- test/test.py
+++ test/test.py
@@ -1,12 +1,27 @@
 from nose.tools import *
-from src.main import import *
-from ctypes.test.test_pickling import name
-from numpy.distutils.conv_template import file
-
-def test_url():
-    file name
-    eq_()
+from src.main import *
     
 def test_read_file():
-    file(read_file(filename))[:4],should be, if wrong)
-    eq_
+    link = "http://claritytrec.ucd.ie/~alawlor/comp30670/input_assign3.txt"
+#     file(read_file(filename))[:4],should be, if wrong)
+    eq_(read_file(link)[:4],"1000","Error")
+    
+# def test_read_lines():
+#     main.main().buffer
+#     i
+     
+# def test_count():
+#     test_grid = main.led(5)
+#     test_grid.switch(0,0,5,5)
+#     eq_(test_grid,"25","Error")
+ 
+def test_turn_on():
+    test_grid = main.led(3)
+    test_grid.switch(0,0,1,1)
+    eq_(test_grid,"4","Error")
+# 
+# def test_turn_off():
+#     pass
+# 
+# def test_switch():
+#     pass
