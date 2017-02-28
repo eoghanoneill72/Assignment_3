06d424eb7f2b8882c3bc1b04ea58d02fe8121153
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-02-28 17:47:07 +0000

    Updated Setup
    for trial on ec2 despite incorrect results

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup
 
-setup(name="ledtester",
+setup(name="led_tester",
       version="0.1",
       description="LED Testing for Assignment3 in COMP30670 2017",
       url="",
