14d7ba55479d5afde16d51da75f9bdb92bb4e1c3
Eoghan O'Neill <eoghan.o-neill.2@ucdconnect.ie>
2017-03-01 13:38:26 +0000

    Test Turn_on added

--- test/test.py
+++ test/test.py
@@ -7,18 +7,19 @@ def test_read_file():
     eq_(read_file(link)[:4],"1000","Error")
     
 # def test_read_lines():
-#     main.main().buffer
-#     i
-     
+#      main.main().buffer
+#      i
+#        
 # def test_count():
-#     test_grid = main.led(5)
-#     test_grid.switch(0,0,5,5)
-#     eq_(test_grid,"25","Error")
+#      test_grid = main.led(5)
+#      test_grid.switch(0,0,5,5)
+#      eq_(test_grid,"25","Error")
  
 def test_turn_on():
-    test_grid = main.led(3)
-    test_grid.switch(0,0,1,1)
-    eq_(test_grid,"4","Error")
+    tester = led(10)
+    tester.turn_on(0,0,9,9)
+    eq_(tester.count(),100,"Error")
+
 # 
 # def test_turn_off():
 #     pass
